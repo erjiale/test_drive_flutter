395f8819c3c90c599914854abef82d62f0c3826a
Jiale Qiu <jiale98chinoguay@gmail.com>
2024-10-17 21:35:17 -0400

    initial commit - i am rich

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          title: const Text('I am Rich', style: TextStyle(color: Colors.blue)),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/IMG_6813.JPG')
+          ),
+        ),
+      ),
+    ),
+  );
+}
